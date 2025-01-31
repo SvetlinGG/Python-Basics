eb8b553674b11f16600f239bf04323db38b4a56e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-01-31 10:25:33 +0100

    basketball equipment

---
+++ first_step_in_coding/basketball_equipment.py
@@ -0,0 +1,7 @@
+year_tax = int(input())
+snickers = year_tax * 0.6
+clothes = snickers * 0.8
+ball = clothes * 0.25
+accessories = ball * 0.2
+total = year_tax + snickers + clothes + accessories + ball
+print(total)
