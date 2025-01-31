afeb5afc21ff8e6dbe2f7eed65d6397fab4441a8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-01-31 02:15:36 +0100

    food delivery

---
+++ first_step_in_coding/food_delivery.py
@@ -0,0 +1,13 @@
+
+
+chiken_count_meal = 2
+price_chiken_meal = chiken_count_meal * 10.35
+fish_count_meal = 4
+price_fish_meal = fish_count_meal * 12.40
+vegeterian_count_meal = 3
+price_vegeterian_meal = vegeterian_count_meal * 8.15
+sum = price_chiken_meal + price_fish_meal + price_vegeterian_meal
+sweet_price = sum * 0.2
+delivery = 2.5
+total_price = sum + sweet_price + delivery
+print(total_price)
