adc807f59bbd4f934e6dc2b388f15b9817951e2a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-02-10 00:41:08 +0100

    bonus scores

--- conditional_statement_exercise/bonus_score.py
+++ conditional_statement_exercise/bonus_score.py
@@ -1,12 +1,18 @@
 first_score = int(input())
 
-bonus_score = ''
+bonus_score = 0
 
-if first_score <=100:
+if first_score <= 100:
     bonus_score = 5
 elif first_score <= 1000:
     bonus_score = first_score * 0.2
 else:
     bonus_score = first_score * 0.1
 
-    print()
+if first_score % 2 == 0:
+    bonus_score = bonus_score + 1
+elif first_score % 5 == 0:
+    bonus_score = bonus_score + 2
+
+    print(bonus_score)
+    print(first_score + bonus_score)
