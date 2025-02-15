4b28ef2e76eb9c4b593f828198e83dd45c6d1500
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-02-15 01:37:27 +0100

    Fruit Shop

--- conditional_statement_advanced_lab/fruit_shop.py
+++ conditional_statement_advanced_lab/fruit_shop.py
@@ -6,34 +6,53 @@ quantity = float(input())
 price = 0
 
 
-if week_day == 'Monday' and week_day == 'Tuesday' and week_day == 'Wednesday' and week_day == 'Thursday' and week_day == 'Friday':
+if week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
     if fruit == 'banana':
-        price = quantity * 2.5
-    elif fruit == 'apple':
-        price = quantity * 1.2
-    elif fruit == 'orange':
-        price = quantity * 0.85
-    elif fruit == 'grapefruit':
-        price = quantity * 1.45
-    elif fruit == 'kiwi':
-        price = quantity * 2.7
-    elif fruit == 'pineapple':
-        price = quantity * 5.5
-    elif fruit == 'grapes':
-        price = quantity * 3.85
-elif week_day == 'Saturday' and week_day == 'Sunday':
+        price = 2.5
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'apple':
+        price = 1.2
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'orange':
+        price = 0.85
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'grapefruit':
+        price = 1.45
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'kiwi':
+        price = 2.7
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'pineapple':
+        price = 5.5
+elif week_day == 'Monday' or week_day == 'Tuesday' or week_day == 'Wednesday' or week_day == 'Thursday' or week_day == 'Friday':
+    if fruit == 'grapes':
+        price = 3.85
+
+
+if week_day == 'Saturday' or week_day == 'Sunday':
     if fruit == 'banana':
-        price = quantity * 2.7
-    elif fruit == 'apple':
-        price = quantity * 1.25
-    elif fruit == 'orange':
-        price = quantity * 0.9
-    elif fruit == 'grapefruit':
-        price = quantity * 1.6
-    elif fruit == 'kiwi':
-        price = quantity * 3.00
-    elif fruit == 'pineapple':
-        price = quantity * 5.6
-    elif fruit == 'grapes':
-        price = quantity * 4.2
-print(f'{price:.2f}')
+        price = 2.7
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'apple':
+        price = 1.25
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'orange':
+        price = 0.9
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'grapefruit':
+        price = 1.6
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'kiwi':
+        price = 3.00
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'pineapple':
+        price = 5.6
+elif week_day == 'Saturday' or week_day == 'Sunday':
+    if fruit == 'grapes':
+        price = 4.2
+
+if price:
+    total = price * quantity
+    print(f'{total:.2f}')
+else:
+    print('error')
