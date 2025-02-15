afc450ffb931d969138e9f8aa4d2287aa92b7715
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-02-16 00:32:56 +0100

    contenue

--- conditional_statement_advanced_exercise/new_house.py
+++ conditional_statement_advanced_exercise/new_house.py
@@ -6,15 +6,16 @@ budget = int(input())
 price = 0
 
 if flowers == 'Roses' and flowers_count > 80:
-        price = ( flowers_count * 5 ) * 0.9
+    price = ( flowers_count * 5 ) * 0.9
 elif flowers == 'Dahlias' and flowers_count > 90:
-        price = ( flowers_count * 3.8 ) * 0.85
+    price = (flowers_count * 3.8) * 0.85
 elif flowers == 'Tulips' and flowers_count > 80:
-        price = ( flowers_count * 2.8 ) * 0.85
+    price = (flowers_count * 2.8) * 0.85
 elif flowers == 'Narcissus' and flowers_count < 120:
-        price = ( flowers_count * 3 ) * 1.15
+    price = (flowers_count * 3) * 1.15
 elif flowers == 'Gladiolus' and flowers_count < 80:
-        price = ( flowers_count * 2.5 ) * 1.2
+    price = (flowers_count * 2.5) * 1.2
+
 
 
 
@@ -24,3 +25,4 @@ if budget >= price:
 else:
     money_needed = price  - budget
     print(f'Not enough money, you need {money_needed:.2f} leva more.')
+
