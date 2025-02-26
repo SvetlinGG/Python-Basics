5f595fc91e0ac78d7e9495acb81dc7131e1f062f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-02-27 00:04:02 +0100

    half sum

--- for_loop_exercise/half_sum_element.py
+++ for_loop_exercise/half_sum_element.py
@@ -1,21 +1,21 @@
-
-from sys import maxsize
+import sys
 
 numbers = int(input())
 
 max_num = -sys.maxsize
 sum_numbers = 0
 
-for _ in range(0, numbers):
+for _ in range(numbers):
     num = int(input())
     if num > max_num:
-        num = max_num
+        max_num = num
     sum_numbers += num
 
-if max_num == sum_numbers - max_num:
+half_sum = sum_numbers - max_num
+
+if half_sum == max_num:
     print('Yes')
     print(f'Sum = {max_num}')
 else:
-    sum_numbers = sum_numbers - max_num
     print('No')
-    print(f'Diff = {abs(max_num - sum_numbers)}')
+    print(f'Diff = {abs(max_num - half_sum)}')
