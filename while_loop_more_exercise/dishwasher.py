b823496a01e548b4001c4effd3fd17b6ccfe0264
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-07 00:20:38 +0100

    different decisions

--- while_loop_more_exercise/dishwasher.py
+++ while_loop_more_exercise/dishwasher.py
@@ -14,7 +14,6 @@ while True:
     if command == 'End':
         break
     dish_count = int(command)
-
     if loads % 3 == 0:
         washed_pots += dish_count
         detergent -=  dish_count * detergent_for_pots
@@ -22,13 +21,15 @@ while True:
     else:
         detergent -= (dish_count * detergent_for_1_dish)
         washed_dishes += dish_count
+    if detergent < 0:
+        print(f'Not enough detergent, {abs(detergent)} ml. more necessary!')
 
 if detergent > 0:
     print(f'Detergent was enough!')
     print(f'{washed_dishes} dishes and {washed_pots} pots were washed.')
     print(f'Leftover detergent {detergent} ml.')
-else:
-    print(f'Not enough detergent, {detergent} ml. more necessary!')
+#else:
+    #print(f'Not enough detergent, {detergent} ml. more necessary!')
 
 
 
