0ad5b233ec6bdf01e0887ab7ba7825ff7d36a11e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-09 19:22:57 +0100

    travelling

--- nested_loops_lab/travelling.py
+++ nested_loops_lab/travelling.py
@@ -1,12 +1,13 @@
-while True:
-    destination = input()
-    if destination == "End":
-        break
-    budget = float(input())
-    money = 0
+# while True:
+#     destination = input()
+#     if destination == "End":
+#         break
+#     budget = float(input())
+#     money = 0
+#
+#     while money < budget:
+#         new_amound = float(input())
+#         money += new_amound
+#
+#     print(f"Going to {destination}!")
 
-    while money < budget:
-        new_amound = float(input())
-        money += new_amound
-
-    print(f"Going to {destination}!")
