c0972123c7a88ec39d6e303dcd635ce681918a6e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-11 09:17:45 +0100

    multiplication

--- nested_loops_lab/mulltyplication_table.py
+++ nested_loops_lab/mulltyplication_table.py
@@ -1,5 +1,5 @@
 
-for x in range(1, 11):
+for x in range(1, 20):
     for y in range(1, 11):
-        product = x * y
-        print(f'{x} * {y} = {product}')
+        result = x * y
+        print(f'{x} * {y} = {result}')
