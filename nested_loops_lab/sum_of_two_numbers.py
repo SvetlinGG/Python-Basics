285a19316a7157dd9dfb75f4a62e30a0b5ac84fb
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-12 12:53:13 +0100

    two numbers finally

--- nested_loops_lab/sum_of_two_numbers.py
+++ nested_loops_lab/sum_of_two_numbers.py
@@ -5,16 +5,21 @@ magic_number = int(input())
 
 combination = 0
 suma = 0
+break_condition = False
 
 for x in range(start_interval, end_interval + 1):
     for y in range(start_interval, end_interval + 1):
+        combination += 1
         suma = x + y
+
         if suma == magic_number:
-            combination += 1
-        if suma == magic_number:
-            print(f'Combination N:{combination} ({x} + {y} = {suma})"')
+            print(f'Combination N:{combination} ({x} + {y} = {suma})')
+            break_condition = True
+            break
+    if break_condition:
+        break
 else:
-    print(f'{combination} combinations - neither equals {magic_number}"')
+    print(f'{combination} combinations - neither equals {magic_number}')
 
 
 
